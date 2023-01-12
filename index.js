c6fa51e49b9a2d6db07c6097aa02a2a9ebb54b0d
kkkovalov <vlad_kovalov@icloud.com>
2023-01-12 15:22:14 -0800

    Connected PostgresSQL database, running locally

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 // Initialization of dependencies
 const express = require('express');
 const _ = require('lodash');
+const dotenv = require('dotenv')
+const { Client } = require('pg');
+
 
 // Express App initialization
 const app = express();
 const {log} = console;
+dotenv.config();
+const client = new Client({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+});
+client.connect((err) => {
+    if(err) console.log(err);
+    else console.log('Database connected');
+});
 
 // index.js constants
 const HOST = 'localhost';
 const PORT = 3000;
 
+
+
 app.listen(PORT, HOST, () => {
     log(`Server is listening on http://${HOST}:${PORT}/ \n\t Ready for connections!`);
+});
+
+app.get('/', (rq, rs) => {
+    client.query('SELECT car_model, price FROM cars;', (err, res) => {
+        const result = res;
+        rs.send(res);
+    });
 });
 
 app.use((req, res) => {
